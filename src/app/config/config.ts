95ad72ef05f83d68e94b036bfc9fa237522fc247
sarin32 <execode02@gmail.com>
2024-02-18 17:07:54 +0530

    linting changes

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,11 +1,4 @@
 // environment level constants
-import * as dotEnv from 'dotenv';
-if (process.env.NODE_ENV !== 'prod') {
-  const configFile = `./src/environments/${process.env.NODE_ENV}.env`;
-  dotEnv.config({path: configFile});
-} else {
-  dotEnv.config();
-}
 
 const env = process.env;
 
